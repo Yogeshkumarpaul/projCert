c12733c5d7fc15412589307e50852f20aa44282d
Yogeshkumarpaul <55760391+Yogeshkumarpaul@users.noreply.github.com>
2019-10-19 23:39:00 +0530

    Update 404.php
    Test to jenkins job

--- website/content/404.php
+++ website/content/404.php
@@ -1,2 +1,2 @@
 <p id="PID-420-pg">404 - The real page is on another server.</p>
-<p id="PID-420-pg">404 - The real page is on another server.</p>
+<p id="PID-420-pg">404 - The real page is on another server address.</p>
